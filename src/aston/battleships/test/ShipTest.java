304947c3febb3ae1572a3ddf75d94ba267b24aa4
sanobar <darsf@aston.ac.uk>
2017-09-18 14:48:25 +0100

    test for the ship class

---
+++ src/aston/battleships/test/ShipTest.java
@@ -0,0 +1,104 @@
+package aston.battleships.test;
+import static org.junit.Assert.*;
+
+import aston.battleships.CellState;
+import aston.battleships.Coordinates;
+import aston.battleships.Ship;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ShipTest {
+    private Ship ship;
+    Coordinates coordinates;
+
+    @Before
+    public void setUp() {
+        coordinates = new Coordinates(1,2);
+        //ship = new ShipImpl(3, coordinates, Orientation.RIGHT);
+    }
+
+
+    @Test
+    public void testGetLength() {
+        assertEquals(ship.getLength(), 3);
+    }
+
+
+    @Test
+    public void testIsAt() {
+        assertTrue(ship.isAt(coordinates));
+    }
+
+    @Test
+    public void testIsAtEndOfShip() {
+        Coordinates endOfShip = new Coordinates(3,2);
+        assertTrue(ship.isAt(endOfShip));
+    }
+
+    @Test
+    public void testIsNotAtRight() {
+        Coordinates noShipRight = new Coordinates(4,2);
+        assertFalse(ship.isAt(noShipRight));
+    }
+
+    @Test
+    public void testIsNotAtDown() {
+        Coordinates noShipDown = new Coordinates(1,3);
+        assertFalse(ship.isAt(noShipDown));
+    }
+
+
+    @Test
+    public void testNoHits() {
+        assertFalse(ship.isSunk());
+    }
+
+    @Test
+    public void testTakeAHit() {
+        ship.takeAHit();
+        assertFalse(ship.isSunk());
+    }
+
+    @Test
+    public void testIsSunk() {
+        ship.takeAHit();
+        ship.takeAHit();
+        ship.takeAHit();
+        assertTrue(ship.isSunk());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testTooManyHits() {
+        ship.takeAHit();
+        ship.takeAHit();
+        ship.takeAHit();
+        ship.takeAHit();
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidLength() {
+        // TODO
+        //new ShipImpl(0, coordinates, Orientation.RIGHT);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullCoordinates() {
+        // TODO
+        //new ShipImpl(3, null, Orientation.RIGHT);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullOrientation() {
+        // TODO
+        //new ShipImpl(3, coordinates, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNegativeCoordinates() {
+        // TODO
+        Coordinates newCoordinates = new Coordinates(-1, 0);
+        //new ShipImpl(3, newCoordinates, Orientation.RIGHT);
+    }
+}
