23bc325d85514d2588804bcff407ab822654c90e
Emily <lawtonel@aston.ac.uk>
2017-09-18 14:58:03 +0100

    completed EnemyBoardImpl

--- src/aston/battleships/EnemyBoardImpl.java
+++ src/aston/battleships/EnemyBoardImpl.java
@@ -19,16 +19,13 @@ public class EnemyBoardImpl implements EnemyBoard {
         }
     }
 
-    // TODO enemy board can only have certain states
     @Override
     public void updateCellState(Coordinates coordinates, CellState newState) {
         coordinates.checkBounds(width, height);
         if(newState == null) {
             throw new IllegalArgumentException("New cell state was null.");
-        } else if(newState == CellState.SHIP_SUNK) {
-            throw new IllegalArgumentException("Ship sunk is not a valid cell state for enemy board.");
-        } else if(newState == CellState.SHIP_NOT_HIT) {
-            throw new IllegalArgumentException("Ship not hit is not a valid cell state for enemy board.");
+        } else if(newState == CellState.SHIP_SUNK || newState == CellState.SHIP_NOT_HIT) {
+            throw new IllegalArgumentException(newState + " is not a valid cell state for enemy board.");
         } else {
             grid[coordinates.x][coordinates.y] = newState;
         }
@@ -41,18 +38,14 @@ public class EnemyBoardImpl implements EnemyBoard {
 
     @Override
     public boolean hasGuessed(Coordinates coordinates) {
+        coordinates.checkBounds(width, height);
         return grid[coordinates.x][coordinates.y] != CellState.NOTHING;
     }
-
     @Override
     public CellState getCellState(Coordinates coordinates) {
-        if(coordinates.x < 0 || coordinates.x >= width || coordinates.y < 0 || coordinates.y >= height) {
-            throw new IllegalArgumentException("Coordinates " + coordinates.x + ", " + coordinates.y + " are out of bounds.");
-        } else {
+        coordinates.checkBounds(width, height);
             return grid[coordinates.x][coordinates.y];
-        }
     }
-
     @Override
     public int getWidth() {
         return width;
