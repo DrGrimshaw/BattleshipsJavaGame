3bf05bf77b0e964bf0024a5d507782342e264570
drgrimshaw <dominic@illusioninteractive.co.uk>
2017-09-18 13:18:24 +0100

    added all test cases

--- src/aston/battleships/test/PlayerBoardTest.java
+++ src/aston/battleships/test/PlayerBoardTest.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
 
 /**
  * Created by cooperwd on 18/09/2017.
@@ -17,33 +19,93 @@ import static junit.framework.TestCase.assertEquals;
 public class PlayerBoardTest {
     PlayerBoard playerBoard;
     Ship ship;
+
     @Before
-    public void setUp(){
-        int width,height;
+    public void setUp() {
+        int width, height;
         width = 7;
         height = 7;
-        playerBoard = null; //new PlayerBoardImpl(width,height);
-        ship = null; //new ShipImpl(length,orientation,startingCoordinates);
+        playerBoard = null; //new PlayerBoardImpl(width, height);
+        ship = null; //new ShipImpl(3, new Coordinates(0,0), Orientation.RIGHT);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void constructIncorrect() {
+        //new PlayerBoardImpl(0,0);
     }
 
     @Test
-    public void getWidthTest(){
-        assertEquals(playerBoard.getWidth(),7);
+    public void getWidthTest() {
+        assertEquals(playerBoard.getWidth(), 7);
     }
 
     @Test
-    public void getHeightTest(){
-        assertEquals(playerBoard.getHeight(),7);
+    public void getHeightTest() {
+        assertEquals(playerBoard.getHeight(), 7);
     }
 
     @Test
-    public void placeShipTest(){
+    public void placeShipTest() {
         playerBoard.placeShip(ship);
     }
 
     @Test
-    public void testGetCellState(){
+    public void testGetCellStateNothing() {
+        assertEquals(playerBoard.getCellState(new Coordinates(0,0)),CellState.NOTHING);
+    }
 
+    @Test
+    public void testGetCellStateMiss() {
+        playerBoard.takeAHit(new Coordinates(0,0));
+        assertEquals(playerBoard.getCellState(new Coordinates(0,0)),CellState.MISS);
+    }
+
+    @Test
+    public void testGetCellStateHit() {
+        playerBoard.placeShip(ship);
+        playerBoard.getShip(new Coordinates(0,0));
+        assertEquals(playerBoard.getCellState(new Coordinates(0,0)),CellState.SHIP_HIT);
+    }
+
+    @Test
+    public void testGetCellStateSunk() {
+        playerBoard.placeShip(ship);
+        playerBoard.takeAHit(new Coordinates(0,0));
+        playerBoard.takeAHit(new Coordinates(0,1));
+        playerBoard.takeAHit(new Coordinates(0,2));
+        assertEquals(playerBoard.getCellState(new Coordinates(0,2)),CellState.SHIP_SUNK);
+    }
+
+    @Test
+    public void testHasGuessedAlready() {
+        playerBoard.takeAHit(new Coordinates(0,0));
+        assertTrue(playerBoard.hasGuessedAlready(new Coordinates(0,0)));
+    }
+
+    @Test
+    public void testShipsRemaining() {
+        playerBoard.placeShip(ship);
+        assertEquals(playerBoard.getNumberOfShipsRemaining(),1);
+    }
+
+    @Test
+    public void testGetShips() {
+        playerBoard.placeShip(ship);
+        assertEquals(playerBoard.getShips().get(0),ship);
+    }
+
+    @Test
+    public void testGameOverFalse() {
+        assertFalse(playerBoard.isGameOver());
+    }
+
+    @Test
+    public void testGameOverTrue() {
+        playerBoard.placeShip(ship);
+        playerBoard.takeAHit(new Coordinates(0,0));
+        playerBoard.takeAHit(new Coordinates(0,1));
+        playerBoard.takeAHit(new Coordinates(0,2));
+        assertTrue(playerBoard.isGameOver());
     }
 
 }
