99d8a9ea2539c1a46337ccab7e75bf95b695b6e0
drgrimshaw <dominic@illusioninteractive.co.uk>
2017-09-20 16:18:04 +0100

    handleTakeAHit() added and created convenience methods for sending and receiving send() and receive()

--- src/aston/battleships/networkclient/ClientProgram.java
+++ src/aston/battleships/networkclient/ClientProgram.java
@@ -1,5 +1,7 @@
 package aston.battleships.networkclient;
 
+import aston.battleships.CellState;
+import aston.battleships.Coordinates;
 import aston.battleships.HumanPlayer;
 import aston.battleships.Player;
 
@@ -34,31 +36,53 @@ public class ClientProgram {
             String command = in.readLine();
             player = handleStart(command);
 
-            while(true) {
+            while (true) {
                 // keep asking the server what it wants us to do: i.e. wait for a command
                 command = in.readLine();
                 // dispatch the command
 
-                if(command.startsWith("PLACE_SHIP")) {
+                if (command.startsWith("PLACE_SHIP")) {
                     handlePlaceShip(command);
-                } else if(command.startsWith("CHOOSE_A_MOVE")) {
+                } else if (command.startsWith("CHOOSE_A_MOVE")) {
                     handleChooseAMove(command);
-                } else if(command.startsWith("TAKE_A_HIT_AT")) {
+                } else if (command.startsWith("TAKE_A_HIT_AT")) {
                     handleTakeAHit(command);
-                } else if(command.startsWith("MOVE_RESPONSE")) {
+                } else if (command.startsWith("MOVE_RESPONSE")) {
                     handleMoveResponse(command);
-                } else if(command.startsWith("GAME_OVER")) {
+                } else if (command.startsWith("GAME_OVER")) {
                     handleGameOver(command);
                 } else {
 
                 }
             }
+        } catch(Player.ResignException e) {
+            // TODO:
+        } catch(Player.QuitException e) {
+            // TODO:
         } catch(IOException e) {
             // TODO: lost connection to server
         } finally {
             if(socket != null) {
                 try { socket.close(); } catch(IOException ignored) {}
             }
+        }
+    }
+
+    private void send(String data) {
+        out.println(data);
+        out.flush();
+    }
+    private void sendf(String data, Object... args) {
+        out.printf(data + "\r\n", args);
+        out.flush();
+    }
+
+    private String receive() throws Player.QuitException {
+        try {
+            return in.readLine();
+        } catch(IOException e) {
+            System.err.println("The client has unexpectedly disconnected");
+            throw new Player.QuitException();
         }
     }
 
@@ -71,7 +95,16 @@ public class ClientProgram {
     private void handleMoveResponse(String command) {
     }
 
-    private void handleTakeAHit(String command) {
+    private void handleTakeAHit(String command) throws Player.QuitException {
+        try {
+            String[] parts = command.split(" ");
+            Coordinates coordinates = new Coordinates(parts[1]);
+            CellState result = player.takeHit(coordinates);
+            send(result.toString());
+        } catch(Coordinates.MalformattedException e) {
+            System.err.println("Error parsing TAKE_A_HIT command: " + command);
+            System.exit(1);
+        }
     }
 
     private void handleChooseAMove(String command) {
