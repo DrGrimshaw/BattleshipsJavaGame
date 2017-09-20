8107e48e56d487a1bbe436e16ce4a72fa0087f98
Nip <dayachan@aston.ac.uk>
2017-09-20 10:10:43 +0100

    Added more on to Game

--- src/aston/battleships/Game.java
+++ src/aston/battleships/Game.java
@@ -1,12 +1,18 @@
 package aston.battleships;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Game {
     Player player1, player2, nextToPlay;
+    List<Integer> shipLengths;
 
     Game(Player newPlayer1, Player newPlayer2){
         player1 = newPlayer1;
         player2 =  newPlayer2;
         nextToPlay = player1;
+        shipLengths = Arrays.asList(5, 4, 3, 3, 2);
     }
 
     public Player getNextToPlay(){
@@ -22,22 +28,38 @@ public class Game {
 
     }
 
+    private void placeShips(){
+        for(int shipLength : shipLengths) {
+            player1.placeShipOnToPlayerBoard(shipLength);
+        }
+        for(int shipLength : shipLengths) {
+            player2.placeShipOnToPlayerBoard(shipLength);
+        }
+    }
+
     public void play() {
         try {
+            placeShips();
+
             while (true) {
+                View view = nextToPlay.getView();
+                nextToPlay.viewState();
                 Coordinates move = nextToPlay.chooseMove();
                 CellState result = getOpponent().takeHit(move);
                 nextToPlay.updateEnemyBoard(move, result);
+                view.viewResultOfMove(result);
 
                 if(getOpponent().hasLost()) {
+                    view.announceGameOver(View.GameOverMessage.WON);
+                    getOpponent().getView().announceGameOver(View.GameOverMessage.LOST);
                     break;
                 }
 
                 nextToPlay = getOpponent();
             }
         } catch(Player.ResignedException e) {
-            //TODO:
-
+            nextToPlay.getView().announceGameOver(View.GameOverMessage.RESIGNED);
+            getOpponent().getView().announceGameOver(View.GameOverMessage.ENEMY_RESIGNED);
         }
     }
 }
