078e60a95059ddefbc69f52db68cabb8b10318fe
drgrimshaw <dominic@illusioninteractive.co.uk>
2017-09-20 16:24:07 +0100

    handlePlaceShip() implemented

--- src/aston/battleships/networkclient/ClientProgram.java
+++ src/aston/battleships/networkclient/ClientProgram.java
@@ -123,6 +123,7 @@ public class ClientProgram {
     }
 
     private void handlePlaceShip(String command) {
-
+        String[] parts = command.split(" ");
+        player.placeShipOnToPlayerBoard(Integer.valueOf(parts[1]));
     }
 }
